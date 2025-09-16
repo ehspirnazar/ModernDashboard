c4a0e8463fad680efa3cf38974dbf7a1f52b6e6f
Ehsan pirnazar <128707241+xmaxehs@users.noreply.github.com>
2025-09-16 12:10:37 +0330

    Add: 404 page, Edit: dashboard sidebar, header

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'
 
-export const metadata = { title: 'E‑Store - Simple E-commerce shop' }
+export const metadata = { title: 'Modern Dahboard with NextJS' }
 
 export default function RootLayout({ children }: { children: React.ReactNode }){
   return (
